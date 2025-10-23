2681d87d6e3d01aeb81090b9eae038597848104d
Erez <erezlahav10@gmail.com>
2025-10-23 20:51:39 +0300

    implement continue and add global debugee pid

--- source/commands.c
+++ source/commands.c
@@ -3,16 +3,27 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include "commands.h"
-
-
-
+#include "debug.h"
+extern RUNNING_STATE proc_state;
+extern pid_t debugee_pid;
 int run_process(int argc,char** argv){
     printf("in run process\n");
 }
 
 int continue_proc(int argc,char** argv){
-    printf("in continue process\n");
-    //ptrace(PTRACE_CONT,pid,NULL,NULL);
+    if(proc_state == LOADED || proc_state == NOT_LOADED){
+        printf("process is not running yet\n");
+        return 0;
+    }
+
+
+    if(debugee_pid != -1){
+        ptrace(PTRACE_CONT,debugee_pid,NULL,NULL);
+        return 1;
+    }
+
+    
+    return 0;
 }
 
 int breakpoint(int argc,char** argv){
